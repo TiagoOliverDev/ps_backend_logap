3c966695adf5e5cd3e561c36684acd935d0aaf0b
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-05-23 18:01:24 -0300

    feat: ajustes gerais

--- app/db/database.py
+++ app/db/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE_URL = os.path.join(BASE_DIR, 'logap.db')
+DATABASE_URL = os.path.join(BASE_DIR, 'logapTeste.db')
 
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{DATABASE_URL}"
 
--- app/models/models.py
+++ app/models/models.py
@@ -15,11 +15,13 @@ class Supplier(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    phone = Column(String, unique=True, index=True)
     products = relationship("Product", back_populates="supplier")
 
 class Product(Base):
     __tablename__ = "products"
-
+    
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     purchase_price = Column(DECIMAL(10, 2))
